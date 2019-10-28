4cc77438e045c512d633de495e0a3cde2719dec3
Luciano Silva <luabsilva@gmail.com>
2019-10-28 17:14:31 -0300

    first commit

---
+++ lista_compras/lib/categorias.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class DrawerCategoriasWidget extends StatefulWidget {
+  @override
+  _DrawerCategoriasWidgetState createState() => _DrawerCategoriasWidgetState();
+}
+
+class _DrawerCategoriasWidgetState extends State<DrawerCategoriasWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        child: Container(
+            width: double.infinity,
+            height: double.infinity,
+            color: Colors.blueAccent));
+  }
+}
